d43d01ca16c2382c89689aa0659ed2e10033efd6
s.tsyb <s.tsyb@apiko.com>
2023-07-19 14:52:41 +0300

    add some data

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,27 +1,34 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image'
+import Link from 'next/link'
 export default function Navigation() {
   return (
     <nav className="py-24 px-12 min-h-full border-r-2">
       <ul>
         <Link href="/">
-          {" "}
-          <li className="py-2 hover:bg-slate-300">Home</li>{" "}
+          {' '}
+          <li className="py-2 hover:bg-slate-300">Home</li>{' '}
         </Link>
         <Link href="/about">
-          {" "}
-          <li className="py-2 hover:bg-slate-300">About</li>{" "}
+          {' '}
+          <li className="py-2 hover:bg-slate-300">About</li>{' '}
+        </Link>
+        <Link href="/posts">
+          {' '}
+          <li className="py-2 hover:bg-slate-300">Blog</li>{' '}
         </Link>
         <Link href="/random-time">
-          {" "}
-          <li className="py-2 hover:bg-slate-300">Get time</li>{" "}
+          {' '}
+          <li className="py-2 hover:bg-slate-300">Get time</li>{' '}
+        </Link>
+        <Link href="/parallel_main">
+          {' '}
+          <li className="py-2 hover:bg-slate-300">Parallel Route</li>{' '}
         </Link>
         <Link href="/empty">
-          {" "}
-          <li className="py-2 hover:bg-slate-300">Not found</li>{" "}
+          {' '}
+          <li className="py-2 hover:bg-slate-300">Not found</li>{' '}
         </Link>
-       
       </ul>
     </nav>
-  );
+  )
 }
